af2c7003ca73790bbb665e6b67c42e7593b3307c
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2023-02-27 16:14:52 +0200

    8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const gallerryItm = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+ `
+  )
+  .join('');
+gallery.innerHTML = gallerryItm;
+
+gallery.addEventListener('click', onGalleryI);
+function onGalleryI(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  new SimpleLightbox('.gallery a', {
+    disableRightClick: true,
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
+form.addEventListener('input', throttle(onInputForm, 1000));
 
+function onInputForm(e) {
+  //   console.log(e.target.value);
+  const valueForm = {
+    Email: input.value,
+    Message: textarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(valueForm));
+}
+form.addEventListener('submit', onSubmitForm);
+
+function ubdate() {
+  let local = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(local);
+  if (local !== null) {
+    input.value = local.Email;
+    textarea.textContent = local.Message;
+  }
+}
+ubdate();
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+}
